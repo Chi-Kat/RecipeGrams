fbaa1c2b1d607a0824c48284a4852771b19ab846
Kathy Li <kathy.j.li@gmail.com>
2012-11-15 11:57:32 -0600

    first commit

---
+++ app/models/recipe.rb
@@ -0,0 +1,10 @@
+
+
+class Recipe < ActiveRecord::Base
+  attr_accessible :ingredient, :instruction, :name
+
+
+
+
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Holidaydrinks::Application.config.secret_token = '58a6d4829b026785e5d0e3f57ff7db9aaa692d026e8be5bd96287d75b59ae1d3cb92eea155cb6a5a142be2c500ec2e31cfef70757f53a1dc97231afc10a75e20'
